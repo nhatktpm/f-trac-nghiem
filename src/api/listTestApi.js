88b21354691fa1dbf264a69b699afe5df8915a48
nhat nguyen <nhathoctap@gmail.com>
2020-12-22 23:43:23 +0700

    setup file api

---
+++ src/api/listTestApi.js
@@ -0,0 +1,27 @@
+import axiosClient from "./axiosClient";
+
+const listTestApi = {
+    getAll(params) {
+        const url = '/list-test';
+        return axiosClient.get(url, { params });
+
+    },
+    get(id) {
+        const url = `/list-test/${id}`;
+        return axiosClient.get(url);
+    },
+    add(data) {
+        const url = '/list-test';
+        return axiosClient.post(url, data);
+    },
+    update(data) {
+        const url = '/list-test/';
+        return axiosClient.put(url, data);
+    },
+    remove(id) {
+        const url = `/list-test/${id}`;
+        return axiosClient.delete(url);
+    }
+};
+
+export default listTestApi;
